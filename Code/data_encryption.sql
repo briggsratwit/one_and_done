f369990c18600c9c061994226baff266fa3a35ed
briggsratwit <54996721+briggsratwit@users.noreply.github.com>
2023-07-27 22:45:21 -0400

    Update data_encryption.sql

--- Code/data_encryption.sql
+++ Code/data_encryption.sql
@@ -3,9 +3,9 @@ select * from students;
 
 -- Modify the contact_number column in students table to be a VARCHAR with a maximum length of 20 characters. 
 -- Updates all the values in the column by hashing each value using the SHA-256 algorithm and converting the resulting hash to binary form using the UNHEX() function.
-ALTER TABLE students MODIFY contact_number VARCHAR(20);
+ALTER TABLE students MODIFY email_address VARBINARY(255);
 UPDATE students
-SET contact_number = UNHEX(SHA2(contact_number, 256));
+SET email_address = UNHEX(SHA2(email_address, 256));
 
 -- Shows all the columns in the students table after the encryption.
-select * from users;
+select * from students;
